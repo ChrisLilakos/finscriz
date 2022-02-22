d1012aa8f994e4db7c66b45a4e76fd8a09b24a45
ChrisLilakos <40068973+ChrisLilakos@users.noreply.github.com>
2022-02-22 18:46:22 -0500

    Update finscriz.py

--- finscriz.py
+++ finscriz.py
@@ -46,7 +46,7 @@ try:  # imports
         browser.implicitly_wait(4)
         browser.find_element_by_link_text("Login").click()
         browser.implicitly_wait(4)
-        browser.find_element_by_css_selector('[name="email"]').send_keys('cjwillis92@yahoo.com')
+        browser.find_element_by_css_selector('[name="email"]').send_keys('c*****is**@yahoo.com')
         browser.minimize_window()
         browser.implicitly_wait(4)
         browser.minimize_window()
@@ -84,7 +84,7 @@ try:  # imports
     shutil.move(path(r"C:\Users\chris\OneDrive\Downloads\finviz.csv"), destinPath)  # MNE!!!!!!!!!!!!!!!!!!!!!!!!!!
 # emailing the csv
     browser.minimize_window()
-    sender, senderAccss, recvr = 'chrigs1ist@gmail.com', input("gm access\n"), 'chrigs1ist@gmail.com'  # MNE!!!!!!!!!
+    sender, senderAccss, recvr = 'c*******st@gmail.com', input("gm access\n"), 'c*******st@gmail.com'  # MNE!!!!!!!!!
     finvMailSubj, outgFile, addtnlTxt = f'finviz csv {todayte}', destinPath, 'sup\n'
     def emailSend(senderIM, senderPC, sendee, subject, bodyAsTxt='', attchmnt=''):
         # there's a way to format/send the body as/in html. its not shown/needed (rn/here) tho.
@@ -161,13 +161,14 @@ except Exception as err:  # text somebody if an exception be up
 # pyAnyW online version:
 # #! python3
 # # finscriz.py - scrapes certain data from finviz and sends an email of it
-# import datetime, sys
-# if datetime.datetime.now().date().strftime("%A") in ["Saturday", "Sunday"]:
-#     sys.exit()
+
+# # if datetime.datetime.now().date().strftime("%A") in ["Saturday", "Sunday"]:
+# #     sys.exit()
 # try:
 # # imports
 #     from pathlib import Path as path
 #     import lxml, itertools, requests, re, shutil, os, time, pwords, traceback
+#     import datetime, sys, wget, zipfile # chromedriver_autoinstaller,
 # # browser start off/up
 #     import undetected_chromedriver as uc
 #     from selenium import webdriver
@@ -179,7 +180,13 @@ except Exception as err:  # text somebody if an exception be up
 #     from selenium.webdriver.chrome.options import Options
 #     from selenium.webdriver.common.by import By as byy  # results in a locator being produced
 #     from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#     # from webdriver_manager.chrome import ChromeDriverManager
+#     # from selenium.webdriver.chrome.service import Service
 #     # print("started")
+
+#     # service = Service(executable_path=ChromeDriverManager().install())
+#     # print(service)
+#     # sys.exit()
 #     def xpath_soup(element):  # copied from github
 #         """Generate xpath of soup element
 #         :param element: bs4 text or node
@@ -198,7 +205,7 @@ except Exception as err:  # text somebody if an exception be up
 #         components.reverse()
 #         return '/%s' % '/'.join(components)
 #     # '# MNE!*' = may have to edit on the pythonAnywhere end. X-MNE-X = pythonAnywhere editing done
-#
+
 #     chrome_options, cwdP = Options(), "/home/JimboDF/finscriz//"  # webdriver.ChromeOptions(), path.cwd()
 #     chromeOptArgLis = ["--headless", "--disable-gpu", "window-size=1536,934", # '--log-path=chromedriver.log', '--kiosk-printing',
 #     "--no-sandbox", "--disable-extensions", '--disable-setuid-sandbox',  '--verbose', '--disable-popup-blocking',
@@ -207,19 +214,22 @@ except Exception as err:  # text somebody if an exception be up
 #     # KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"""]
 #     for optArg in chromeOptArgLis:
 #         chrome_options.add_argument(optArg)
-#
+
 #     # set download directory path and adding preferences to ChromeOptions
 #     pref, waitSecs = {"download.default_directory":f"{cwdP}", "download.prompt_for_download": False,
 #     'savefile.default_directory': f"{cwdP}", 'profile.default_content_settings.popups': 0, "download.directory_upgrade": True,
 #     "download.download_restrictions": 0, "safebrowsing_for_trusted_sources_enabled": False, "safebrowsing.enabled": False,
 #     'safebrowsing.disable_download_protection': True}, 5
-#
+
 #     chrome_options.add_experimental_option("prefs", pref)
 #     chrome_options.experimental_options["prefs"] = pref  # same thing?
 #     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 #     chrome_options.add_experimental_option('useAutomationExtension', False)  # bulk comments line line 60 or 61
-#     browser = uc.Chrome(options=chrome_options)  # "home/JimboDF/finscriz/chromedriver", # bulk comments line 58, 60, or 61
-#
+#     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#     # chromedriver_autoinstaller.install(cwd=True)
+#     browser = uc.Chrome(executable_path="/home/JimboDF/finscriz/chromedriver", options=chrome_options)
+#         # , service=Service(ChromeDriverManager().install()), # bulk comments line 58, 60, or 61
+
 #     browser.execute_cdp_cmd('Page.setDownloadBehavior', {'behavior': 'allow', 'downloadPath': f"{cwdP}"})
 #         # formerly it was , params and params=what the 2nd parram is
 #     def enable_download_in_headless_chrome(browser, download_dir):
@@ -242,8 +252,18 @@ except Exception as err:  # text somebody if an exception be up
 #             secs -= 1
 #         # wait.until(ec.presence_of_element_located((byy.findBy, findStr)))
 #         browser.implicitly_wait(secs)
-# # logging in
+# # the markets open today?
 #     browser.maximize_window()
+#     browser.get("https://www.tradinghours.com/open?")
+#     # if browser.find_element(byy.CLASS_NAME, "text-open").find_element(byy.TAG_NAME, 'u').text == "No":
+#         # element may not have text-open class when the markets are closed
+#     if browser.find_element(byy.XPATH, "/html/body/main/div[2]/p[1]/span/u").text == "No":
+#         # print("\n\nn\n\n")
+#         sys.exit()
+#     # else:
+#         # print("\n\ny\n\n")
+#         # sys.exit()
+# # logging in
 #     browser.get(finLink)
 #     # try:
 #     def finvCjLogin():
@@ -253,7 +273,7 @@ except Exception as err:  # text somebody if an exception be up
 #         wait.until(ec.element_to_be_clickable((byy.LINK_TEXT, "Login")))
 #         browser.find_element(byy.LINK_TEXT, "Login").click()  # find_element makes the locator a webElement
 #         browser.implicitly_wait(4)
-#         browser.find_element(byy.CSS_SELECTOR, '[name="email"]').send_keys('cjwillis92@yahoo.com')
+#         browser.find_element(byy.CSS_SELECTOR, '[name="email"]').send_keys('c*****is**@yahoo.com')
 #         browser.minimize_window()
 #         browser.implicitly_wait(4)
 #         browser.minimize_window()
@@ -265,24 +285,24 @@ except Exception as err:  # text somebody if an exception be up
 #         browser.get(finLink)
 #         megaWait(waitSecs)
 #     finvCjLogin()
-#
+
 # # exporting and moving the csv.   # bulk comments for under this section
 #     browser.execute_script('document.getElementsByClassName("tab-link")[5].target = "_self";')
 #     exportLink = browser.find_element(byy.LINK_TEXT, 'export').get_attribute("href")
 #     browser.get(exportLink)
-#
+
 #     browser.execute_script('document.getElementsByClassName("tab-link")[5].target = "_self";')
 #     browser.find_element(byy.LINK_TEXT, 'export').click()  # bulk comments line 131
 #     browser.execute_script('window.open = function(url) {window.location=url}')  #add 'self.' to the beginning?
-#
+
 #     megaWait(waitSecs)  # wait for file to download.
 #     destinPath = path(path.cwd() / f'finvizCSV{todayte}.csv')  # destinPath needs to be the path of what the csv WILL be
 #     # browser.get_screenshot_as_file("screen_snap.png")
-#
+
 #     shutil.move(path(cwdP + "finviz.csv"), destinPath)  # bulk comments line 143
 # # emailing the csv
 #     browser.minimize_window()
-#     sender, senderAccss, recvr = 'chrigs1ist@gmail.com', pwords.chrigslist_password, 'cjwillis92@yahoo.com'
+#     sender, senderAccss, recvr = 'c*******st@gmail.com', pwords.chrigslist_password, 'c*****is**@yahoo.com'
 #     finvMailSubj, outgFile, addtnlTxt = f'finviz csv {todayte}', destinPath, 'sup\n'  # change the body here if need be
 #     def emailSend(senderIM, senderPC, sendee, subject, bodyAsTxt='', attchmnt=''):
 #         # there's a way to format/send the body(bodyAsTxt/addtnlTxt) as/in html. its not shown/needed (rn/here) tho.
@@ -316,30 +336,35 @@ except Exception as err:  # text somebody if an exception be up
 #         if attchmnt:
 #             time.sleep(2)
 #             openAtt.close()
-#     emailSend(sender, senderAccss, recvr, finvMailSubj, bodyAsTxt=addtnlTxt, attchmnt=str(outgFile))
+#     emailSend(sender, senderAccss, recvr, finvMailSubj, bodyAsTxt=addtnlTxt, attchmnt=str(outgFile))  # ------------------------------------------
 # # final touches  # bulk comments for under this section
 #     os.remove(destinPath)  # maybe move to the finally clause
 # except Exception as err:  # text somebody if an exception be up  # bulk comments within line 183's exception
-#     from twilio.rest import Client  # correct way to access twilio mod
-#     chrisLCell, CMLsTwilioNum = '3475673868', '+14632223597'  # X-MNE-X
-#     twilioMyGetIn = Client(pwords.CMLtwilio_sid, pwords.CMLtwilio_token)  # X-MNE-X
-#     fullErr = f"""at {datetime.datetime.now()} an error occurred:\n\n \tError Traceback: \n\t\t{traceback.format_exc()}
-#     \n\n Error: {err}"""
-#     message = twilioMyGetIn.messages.create(body=f"error with the code:\n\n{fullErr}", from_=CMLsTwilioNum, to=chrisLCell)
-#     sid = message.sid
-#     time.sleep(15)  # wait a few seconds to see if the msg sent
-#     print(twilioMyGetIn.messages(sid).fetch().status)  # if dis don't werk eider, den I.D.K. What.
-#     #     # could nest another try/except statement and have it send me an email but im feeling like going
-#     #     # down dat path opens up a rabbit hole. does pythonAnywhere send alerts when there's errors?
+#     if "sys.exit()" in f'{traceback.format_exc()}':
+#         sys.exit()
+#     def twilioRun(errr):
+#         from twilio.rest import Client  # correct way to access twilio mod
+#         chrisLCell, CMLsTwilioNum = '3475673868', '+14632223597'  # X-MNE-X
+#         twilioMyGetIn = Client(pwords.CMLtwilio_sid, pwords.CMLtwilio_token)  # X-MNE-X
+#         fullErr = f"""at {datetime.datetime.now()} an error occurred:\n\n \tError Traceback (last 700 characters):
+#         \t\t{traceback.format_exc()[-700:]}\n\n Error: {errr}"""
+#         message = twilioMyGetIn.messages.create(body=f"error with the code:\n\n{fullErr}", from_=CMLsTwilioNum, to=chrisLCell)
+#         sid = message.sid
+#         time.sleep(15)  # wait a few seconds to see if the msg sent
+#         print(twilioMyGetIn.messages(sid).fetch().status)  # if dis don't werk eider, den I.D.K. What.
+#         #     # could nest another try/except statement and have it send me an email but im feeling like going
+#         #     # down dat path opens up a rabbit hole. does pythonAnywhere send alerts when there's errors?
+#     twilioRun(err) # ------------------------------------------------------------------------------------------------------------
+#     # print(f"{traceback.format_exc()}\n\n Error: {err}")
 # finally:
 #     time.sleep(10)
-#     browser.quit()  # is this different then .close? yes cuz this will close all browser windows and
-#         # terminates the server processes connection or blah blah something like that, goog it
+#     if "browser" in locals():
+#         browser.quit()  # is this different then .close? yes cuz this will close all browser windows and
+#             # terminates the server processes connection or blah blah something like that, goog it
 #     # print("fin", datetime.datetime.now())
 #     # sys.exit() # ------------------------------------------------------
-#     # fin.
-#
-#
+
+
 # # bulk ununcommentabls: *line numbers and code on that line are subject to change.*
 #     # line 58 - browser = uc.Chrome(options=chrome_options):
 #             # , service_args=["--log-path=./Logs/DubiousDan.log"]
@@ -412,5 +437,7 @@ except Exception as err:  # text somebody if an exception be up
 #         # potential other answers for python selenium headless download:
 #             # https://bugs.chromium.org/p/chromedriver/issues/detail?id= 2454, 3120, 3548
 #         # browser = webdriver.Chrome(options=chrome_options
-#
+
+# # fin. -------------------------------------------------------------------------------------------------------------------------------
+# #
 # # -------------------------------------------------------------------------------------------------------------------------------
